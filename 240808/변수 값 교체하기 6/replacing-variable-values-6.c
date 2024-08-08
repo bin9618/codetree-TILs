cf25707f3f8ff784516d2abb74e6011fa73c1566
bin9618 <wkss18@gmail.com>
2024-08-08 17:14:46 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/변수 값 교체하기 6/replacing-variable-values-6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 2, b = 5;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    return 0;
+}
